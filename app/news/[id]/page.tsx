581d82e05327299e74d550ad445746ef2b6638a2
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2024-04-03 19:44:33 +0600

    u

--- app/news/[id]/page.tsx
+++ app/news/[id]/page.tsx
@@ -6,6 +6,51 @@ import { useParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
 
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
+
+const ImageSlider = ({ slides }) => {
+
+    if (!slides) {
+        return null;
+    }
+
+    const [current, setCurrent] = useState(0);
+    const length = slides.length;
+
+    const nextSlide = () => {
+        setCurrent(current === length - 1 ? 0 : current + 1);
+    };
+
+    const prevSlide = () => {
+        setCurrent(current === 0 ? length - 1 : current - 1);
+    };
+
+    if (!Array.isArray(slides) || slides.length <= 0) {
+        return null;
+    }
+
+    return (
+        <section className='slider'>
+            <button className="left-arrow" onClick={prevSlide}>←</button>
+            <button className="right-arrow" onClick={nextSlide}>→</button>
+            {slides.map((slide, index) => {
+                return (
+                    <div
+                        className={index === current ? 'slide active' : 'slide'}
+                        key={index}
+                    >
+                        {index === current && (
+                            <img src={slide} alt='travel image' className='image' />
+                        )}
+                    </div>
+                );
+            })}
+        </section>
+    );
+};
+
+
+
 export default function SomeClientComponent() {
     const { id } = useParams();
     const [data, setData] = useState(null);
@@ -15,6 +60,7 @@ export default function SomeClientComponent() {
             let { news } = await getNewsById(id);
             if (news) {
                 setData(news[0]);
+                console.log(news[0]);
             }
 
         }
@@ -30,18 +76,19 @@ export default function SomeClientComponent() {
     return (
         <div className="w-full mx-auto flex  items-center flex-col mt-8 p-4 bg-white rounded-lg ">
             <div className="max-w-lg">
-            <img className="w-full rounded" src={data?.cover_photo_url} />
-            <h1 className="text-2xl font-bold mb-4 mt-3">{data?.title}</h1>
+                <img className="w-full rounded" src={data?.cover_photo_url} />
+                <h1 className="text-2xl font-bold mb-4 mt-3">{data?.title}</h1>
 
             </div>
             <div >
                 <Preview markdown={data?.content} />
             </div>
+            <ImageSlider slides={data?.photos} />
             <div className='w-full'>
                 <h2 className="text-lg font-semibold mb-2">Tags</h2>
                 <ul className="flex flex-wrap w-full">
-                    {data?.tags.map(tag => (
-                        <li key={tag.id} className="mr-2 mb-2 bg-gray-800 text-white rounded p-2 font-bold ">{tag.text}</li>
+                    {data?.tags.map((tag, i) => (
+                        <li key={i} className="mr-2 mb-2 bg-gray-800 text-white rounded p-2 font-bold ">{tag.text}</li>
                     ))}
                 </ul>
             </div>
